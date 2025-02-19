db59bf596ba598c63a7b0e23644ed40a08ea5559
ymh1995s <2014038117ab@gmail.com>
2025-02-20 07:56:59 +0900

    프로토버프 CPP 환경 구축 완료

--- GameServer/GameServer/pch.h
+++ GameServer/GameServer/pch.h
@@ -3,8 +3,8 @@
 #define WIN32_LEAN_AND_MEAN // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
 
 #ifdef _DEBUG
-#pragma comment(lib, "Debug\\ServerCore.lib")
-//#pragma comment(lib, "Protobuf\\Debug\\libprotobufd.lib")
+#pragma comment(lib, "ServerCore\\Debug\\ServerCore.lib")
+#pragma comment(lib, "Protobuf\\Debug\\libprotobufd.lib")
 #else
 #pragma comment(lib, "ServerCore\\Release\\ServerCore.lib")
 #pragma comment(lib, "Protobuf\\Release\\libprotobuf.lib")
